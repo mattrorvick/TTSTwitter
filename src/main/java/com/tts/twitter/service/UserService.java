d5e117364a06c4351027e7f5c88856eeb2e74b70
mattrorvick <matthewrorvick@gmail.com>
2021-02-25 10:10:52 -0600

    Update UserService

--- src/main/java/com/tts/twitter/service/UserService.java
+++ src/main/java/com/tts/twitter/service/UserService.java
@@ -1,12 +1,16 @@
 package com.tts.twitter.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
+import com.tts.twitter.model.Role;
 import com.tts.twitter.model.User;
 import com.tts.twitter.repository.RoleRepository;
 import com.tts.twitter.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +40,19 @@ public class UserService {
 
     public void save(User user) {
         userRepository.save(user);
+    }
+
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        return findByUsername(loggedInUsername);
     }
 
 }
