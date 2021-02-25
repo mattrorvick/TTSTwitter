eefc7bfdf363fa1c62892f51c15f32beb90608e4
mattrorvick <matthewrorvick@gmail.com>
2021-02-25 09:15:38 -0600

    Update Role

---
+++ src/main/java/com/tts/twitter/model/Role.java
@@ -0,0 +1,47 @@
+package com.tts.twitter.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", role=" + role + "]";
+    }
+
+    
+    
+    
+}
